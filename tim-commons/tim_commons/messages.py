fe3538f797a5583c30ae3139aff86545f364b6ce
Howard Burrows <howard@mobileidentity.me>
2012-05-07 13:50:16 -0700

    Updating message creation to simpler non OO implementation

--- tim-commons/tim_commons/messages.py
+++ tim-commons/tim_commons/messages.py
@@ -4,113 +4,121 @@ Created on May 7, 2012
 @author: howard
 '''
 
-import json
-
-
 '''
-  Root for all messages.  Encapsulates standard header and message 
-  functionality
+  Root for all messages.  Encapsulates standard header and message
+  definition.  Message are just dictionaries so only the control
+  of their creation is enforced.
 '''
 
-class TIMMessage(object):
 
-  def __init__(self):
-    self.jsonDict = None
-
-  # Formats the message dictionary.  All messages must have a header component and
-  # a message component
-  def create(self,version,message_type):
-    self.jsonDict['header'] = { 'version': version, 'type': message_type }
-    self.jsonDict['message'] = None
-    return self
-
-  # Get the message as a JSON dictionary
-  def getJSONDict(self):
-    if self.jsonDict:
-      raise Exception('Improper usage. Message not yet initialized')
-    return self.jsonDict
-
-  # Get the message as a JSON formatted string
-  def toJSON(self):
-    if self.jsonDict:
-      raise Exception('Improper usage. Message not yet initialized')
-    return json.dumps(self.jsonDict,sort_keys=True, indent=2)
+def _create_tim_message(version, message_type):
+  return {'header': {'version': version, 'type': message_type},
+          'message': None}
 
 
 '''
   Notification message format.
-  
+
   Represents messages requesting retrieval of new events for the specified user
 '''
-  
-class NotificationMessage(TIMMessage):
-  
-  def create(self, version, message_type, tim_author_id, service_author_id, oauth_access_token, oauth_access_token_secret):
-    super(NotificationMessage, self).create(version,message_type)
-    self.jsonDict['message'] = { 'tim_author_id':tim_author_id,
-                                 'service_author_id':service_author_id,
-                                 'oauth_access_token':oauth_access_token,
-                                 'oauth_access_token_secret':oauth_access_token_secret }
-    return self
 
 
-class FacebookNotification(NotificationMessage):
+def _create_notification_message(message_type,
+                                 tim_author_id,
+                                 service_author_id,
+                                 oauth_access_token, oauth_access_secret):
 
   VERSION = 1
+
+  json_dict = _create_tim_message(VERSION, message_type)
+
+  json_dict['message'] = {'tim_author_id': tim_author_id,
+                          'service_author_id': service_author_id,
+                          'oauth_access_token': oauth_access_token,
+                          'oauth_access_secret': oauth_access_secret}
+
+  return json_dict
+
+
+def create_facebook_notification(facebook_user_id,
+                                 tim_author_id=None,
+                                 oauth_access_token=None,
+                                 auth_access_secret=None):
+
   TYPE = 'facebook.notification'
 
-  def create(self,facebook_user_id, tim_author_id=None, oauth_access_token=None, auth_access_token_secret=None):
-    super(FacebookNotification, self).create(self.VERSION,self.TYPE,tim_author_id, facebook_user_id, oauth_access_token, auth_access_token_secret)
-    return self
-
-
+  return _create_notification_message(TYPE,
+                                      tim_author_id,
+                                      facebook_user_id,
+                                      oauth_access_token, auth_access_secret)
 
 '''
   Raw service event message.
-  
-  Holds messages containing raw service events that need to be added or updated in the system.
+
+  Holds messages containing raw service events that need to be added or updated
+  in the system.
 '''
 
-class ServiceEventMessage(TIMMessage):
 
-  def create(self, version, message_type, tim_author_id, service_author_id, json_dict):
-    super(ServiceEventMessage, self).create(version, message_type)
-    self.jsonDict['message'] = { 'tim_author_id':tim_author_id,
-                                 'service_author_id':service_author_id,
-                                 'service_event_json':json_dict }
-
-
-class FacebookEventMessage(ServiceEventMessage):
+def _create_event_message(message_type,
+                          tim_author_id,
+                          service_author_id,
+                          json_event_dict):
 
   VERSION = 1
+
+  json_dict = _create_tim_message(VERSION, message_type)
+
+  json_dict['message'] = {'tim_author_id': tim_author_id,
+                          'service_author_id': service_author_id,
+                          'service_event_json': json_event_dict}
+
+  return json_dict
+
+
+def create_facebook_event(facebook_user_id,
+                          tim_author_id,
+                          json_event_dict):
+
   TYPE = 'facebook.event'
 
-  def create(self, tim_author_id, facebook_user_id, json_dict):
-    super(FacebookEventMessage, self).create(self.VERSION,self.TYPE,tim_author_id, facebook_user_id, json_dict)
-    return self
-
+  return _create_event_message(TYPE,
+                               tim_author_id,
+                               facebook_user_id,
+                               json_event_dict)
 
 
 '''
   Update event message.
-  
-  Stores messages containing event identifiers that need to be checked for updates
+
+  Stores messages containing event identifiers that need to be checked for
+  updates.
 '''
 
-class UpdateEventMessage(TIMMessage):
 
-  def create(self, version, message_type, tim_author_id, service_author_id, service_event_id):
-    super(ServiceEventMessage, self).create(version, message_type)
-    self.jsonDict['message'] = { 'tim_author_id':tim_author_id,
-                                 'service_author_id':service_author_id,
-                                 'service_event_id':service_event_id }
-
-class FacebookUpdateEventMessage(UpdateEventMessage):
+def _create_event_update_message(message_type,
+                                 tim_author_id,
+                                 service_author_id,
+                                 service_event_id):
 
   VERSION = 1
+
+  json_dict = _create_tim_message(VERSION, message_type)
+
+  json_dict['message'] = {'tim_author_id': tim_author_id,
+                          'service_author_id': service_author_id,
+                          'service_event_id': service_event_id}
+
+  return json_dict
+
+
+def create_facebook_event_update(facebook_user_id,
+                                 tim_author_id,
+                                 facebook_event_id):
+
   TYPE = 'facebook.update'
 
-  def create(self, tim_author_id, facebook_user_id, facebook_event_id):
-    super(FacebookEventMessage, self).create(self.VERSION,self.TYPE,tim_author_id, facebook_user_id, facebook_event_id)
-    return self
-
+  return _create_event_message(TYPE,
+                               tim_author_id,
+                               facebook_user_id,
+                               facebook_event_id)
