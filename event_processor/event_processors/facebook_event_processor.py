649fedc6262bbcc3ada799f8bfd7ce704aeb7982
Howard Burrows <howard@mobileidentity.me>
2012-06-20 15:19:12 -0700

    Added FacebookCheckinInterpreter support

--- event_processor/event_processors/facebook_event_processor.py
+++ event_processor/event_processors/facebook_event_processor.py
@@ -7,6 +7,8 @@ Created on May 8, 2012
 from event_interpreter.facebook_event_interpreter import FacebookStatusEventInterpreter
 from event_interpreter.facebook_event_interpreter import FacebookPhotoAlbumEventInterpreter
 from event_interpreter.facebook_event_interpreter import FacebookPhotoEventInterpreter
+from event_interpreter.facebook_event_interpreter import FacebookCheckinInterpreter
+
 from event_processor import EventProcessor
 
 
@@ -18,5 +20,7 @@ class FacebookEventProcessor(EventProcessor):
       return FacebookPhotoAlbumEventInterpreter(service_event_json, author_service_map, oauth_config)
     elif event_type == 'photo':
       return FacebookPhotoEventInterpreter(service_event_json, author_service_map, oauth_config)
+    elif event_type == 'checkin':
+      return FacebookCheckinInterpreter(service_event_json, author_service_map, oauth_config)
     else:
       return FacebookStatusEventInterpreter(service_event_json, author_service_map, oauth_config)
