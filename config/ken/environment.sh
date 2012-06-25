054dda7559a65139dd0e2595689cdda1caa9734b
Jose Garcia <jose@thisis.me>
2012-06-25 15:11:07 -0400

    Move the event interpreter code to commons

--- config/ken/environment.sh
+++ config/ken/environment.sh
@@ -20,7 +20,6 @@ export TIM_OAUTH_KEYS=$TIM_CONFIG/oauth_keys.json
 export PYTHONPATH=$TIM_HOME/mi-utils:$TIM_HOME/mi-model:$TIM_HOME/mi-db
 export PYTHONPATH=$TIM_HOME/mi-collectors:$TIM_HOME/mi-traversal:$PYTHONPATH
 export PYTHONPATH=$TIM_HOME/tools:$TIM_HOME/commons:$TIM_HOME/event_scanner:$PYTHONPATH
-export PYTHONPATH=$TIM_HOME/event_interpreter:$PYTHONPATH
 
 # define s3 scripts bucket
 export SCRIPT_BUCKET=scripts.dev.thisis.me
