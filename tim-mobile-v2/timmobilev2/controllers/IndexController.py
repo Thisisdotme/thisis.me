1e8dd9348f21d363bb2c5833dcf9e434ec978557
Howard Burrows <howard@mobileidentity.me>
2012-06-25 09:47:42 -0700

    Fixing missing db commit/rollbacks resulting in dangling open transactions

--- tim-mobile-v2/timmobilev2/controllers/IndexController.py
+++ tim-mobile-v2/timmobilev2/controllers/IndexController.py
@@ -7,22 +7,17 @@ import logging
 
 from pyramid.view import view_config
 
-from timmobilev2.models import DBSession
-
 log = logging.getLogger(__name__)
 
+
 class IndexController(object):
-  '''
-  classdocs
-  '''
-  
+
   '''
   Constructor
   '''
   def __init__(self, request):
     self.request = request
-    self.dbSession = DBSession()
 
   @view_config(route_name='index', request_method='GET', renderer='timmobilev2:templates/index.pt')
   def requestHandler(self):
-    return { 'api_endpoint':self.request.registry.settings['mi.api.endpoint'] }    
+    return {'api_endpoint': self.request.registry.settings['mi.api.endpoint']}
