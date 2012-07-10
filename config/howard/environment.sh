74f92d5798ec6ebd53c936369c083bfa560c0569
Howard Burrows <howard@mobileidentity.me>
2012-07-10 14:38:28 -0700

    Updating tim-mobile to use common configuration

--- config/howard/environment.sh
+++ config/howard/environment.sh
@@ -17,8 +17,7 @@ export TIM_CONFIG=$TIM_HOME/config/$environment
 export TIM_OAUTH_KEYS=$TIM_CONFIG/oauth_keys.json
 
 # Define the pythonpath
-export PYTHONPATH=$TIM_HOME/mi-utils:$TIM_HOME/mi-model:$TIM_HOME/mi-db
-export PYTHONPATH=$TIM_HOME/mi-collectors:$TIM_HOME/mi-traversal:$PYTHONPATH
+export PYTHONPATH=$TIM_HOME/mi-model:$TIM_HOME/mi-db
 export PYTHONPATH=$TIM_HOME/tools:$TIM_HOME/commons:$TIM_HOME/event_scanner:$PYTHONPATH
 
 # define s3 scripts bucket
