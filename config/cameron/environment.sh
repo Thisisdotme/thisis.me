435effb6808c8dee78d87dd60809e1721310f700
Cameron Drake <cdrake757@gmail.com>
2012-07-10 16:11:20 -0700

    Added Cameron to the Developer environment

---
+++ config/cameron/environment.sh
@@ -0,0 +1,24 @@
+environment=howard
+
+# Define the root location for all mobile identity scripts, sourcec code, config
+# files, etc.
+export TIM_HOME=/Users/design/dev/thisis.me
+
+# Soure the python virtual environment
+. $TIM_HOME/env/bin/activate
+
+# Define root location for data file
+export TIM_DATA=$TIM_HOME/data/$environment
+
+# Define location of configuration files
+export TIM_CONFIG=$TIM_HOME/config/$environment
+
+# Define oath keys
+export TIM_OAUTH_KEYS=$TIM_CONFIG/oauth_keys.json
+
+# Define the pythonpath
+export PYTHONPATH=$TIM_HOME/mi-model:$TIM_HOME/mi-db
+export PYTHONPATH=$TIM_HOME/tools:$TIM_HOME/commons:$TIM_HOME/event_scanner:$TIM_HOME/profile_fetcher:$PYTHONPATH
+
+# define s3 scripts bucket
+export SCRIPT_BUCKET=scripts.dev.thisis.me
