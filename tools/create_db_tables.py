a7d5225738ad7a095acc11864a3b4ab7446caa3e
Howard Burrows <howard@mobileidentity.me>
2012-07-18 16:49:48 -0700

    New utility script to create database tables

---
+++ tools/create_db_tables.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import sys
+import logging
+
+from tim_commons.app_base import AppBase
+from tim_commons import db
+
+import mi_schema.models as mi_schema
+
+
+class CreateDBTables(AppBase):
+
+  def app_main(self, config, options, args):
+
+    logging.info("Beginning...")
+
+    # read the db url from the config
+    db_url = config['db']['sqlalchemy.url']
+
+    # initialize the db engine & session
+    db.configure_session(db_url)
+
+    # get the engine, bind it to the metadata base, and create
+    # the tables
+    engine = db.Session().get_bind()
+
+    mi_schema.Base.metadata.bind = engine
+    mi_schema.Base.metadata.create_all(engine)
+
+    logging.info("Finished...")
+
+if __name__ == '__main__':
+  # Initialize with number of arguments script takes
+  sys.exit(CreateDBTables('create_db_tables').main())
