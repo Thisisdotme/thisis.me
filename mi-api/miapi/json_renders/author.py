7672b5309c9c8b7c155156272465a3b3884a17db
Howard Burrows <howard@thisis.me>
2012-08-09 12:45:23 -0700

    Adding new json_renders package to API and associated author renderer; remove to_JSON from mi_schema.models

---
+++ mi-api/miapi/json_renders/author.py
@@ -0,0 +1,32 @@
+'''
+Created on Aug 9, 2012
+
+@author: howard
+'''
+
+from sqlalchemy import and_
+import sqlalchemy.orm.exc
+
+from tim_commons import db
+import mi_schema.models
+import data_access.service
+
+
+def to_person_fragment_JSON_dictionary(author, author_service_map=None):
+  JSON_dict = {'service_name': 'me',
+               'id': author.id,
+               'name': author.author_name,
+               'full_name': author.full_name}
+  if not author_service_map:
+    try:
+      author_service_map = db.Session().query(mi_schema.models.AuthorServiceMap). \
+                                        filter(and_(mi_schema.models.AuthorServiceMap.author_id == author.id,
+                                                    mi_schema.models.AuthorServiceMap.service_id == data_access.service.name_to_id("me"))). \
+                                        one()
+    except sqlalchemy.orm.exc.NoResultFound:
+      pass
+
+  if author_service_map.profile_image_url:
+    JSON_dict['picture'] = {'image_url': author_service_map.profile_image_url}
+
+  return JSON_dict
